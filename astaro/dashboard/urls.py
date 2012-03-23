3d20cb19f8026ce29e15584a19bcbed30b9d3d34
Django <django@omd.fela.ch>
2012-03-23 12:32:12 +0100

    Sample chart added

--- astaro/dashboard/urls.py
+++ astaro/dashboard/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls.defaults import patterns, url
 urlpatterns = patterns('dashboard.views',
     url(r'^$', 'dashboard', name='dashboard'),
     url(r'^search$', 'search_page', name='dashboard_search'),
+    url(r'^chart/$', 'chart_view', name='dashboard_chart'),
     url(r'^ajax/autocomplete$', 'ajax_autocomplete', name='dashboard_autocomplete'),
 )
