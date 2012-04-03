ff7f20a38603bb0d4042a698958c5e8d92494c62
Django <django@omd.fela.ch>
2012-04-03 19:00:56 +0200

    Simple livestatus query object

--- astaro/dashboard/views.py
+++ astaro/dashboard/views.py
@@ -8,6 +8,22 @@ from datetime import date
 import paramiko
 
 
+class LiveStatus(object):
+
+    def __init__(self, server, username):
+        self.server = server
+        self.username = username
+        self.ssh = paramiko.SSHClient()
+        self.ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        self.ssh.connect(server, username=username)
+
+    def query(self, query):
+        cmd = 'cat <<"EOF" | unixcat /var/lib/nagios3/rw/live\n%s\nResponseHeader: fixed16\nEOF' % query
+        ssh_stdin, ssh_stdout, ssh_stderr = self.ssh.exec_command(cmd)
+        data = ssh_stdout.read()
+        return data
+
+
 def dashboard(request):
     context = {}
     context['nav_name'] = 'dashboard'
@@ -23,15 +39,22 @@ def dashboard(request):
     context['drops'] = PacketFilter.objects.filter(logday__gte=thismonth, logday__lt=nextmonth).values('logday').annotate(tot=Sum('packets')).order_by('logday')[:35]
 
     ################################################################################
-    server, username = ('192.168.139.104', 'root')
-    cmd = "unixcat < livestatus-test/query.txt /var/lib/nagios3/rw/live"
-    ssh = paramiko.SSHClient()
-    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh.connect(server, username=username)
-    ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(cmd)
-    data = ssh_stdout.read()
+    livestatus = LiveStatus('192.168.139.104', 'root')
+    data = livestatus.query("""GET hostgroups
+Filter: name = _arco-ica-automaten
+Columns: num_hosts num_hosts_up num_hosts_down num_hosts_unreach num_hosts_pending num_services num_services_ok num_services_crit num_services_warn num_services_unknown num_services_pending"""
+)
     context['live'] = data.split()[2].split(';')
-    ssh.close()
+    ################################################################################
+    # server, username = ('192.168.139.104', 'root')
+    # cmd = "unixcat < livestatus-test/query.txt /var/lib/nagios3/rw/live"
+    # ssh = paramiko.SSHClient()
+    # ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    # ssh.connect(server, username=username)
+    # ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(cmd)
+    # data = ssh_stdout.read()
+    # context['live'] = data.split()[2].split(';')
+    # ssh.close()
     ################################################################################
 
     return render_to_response('dashboard/index.html', context,
